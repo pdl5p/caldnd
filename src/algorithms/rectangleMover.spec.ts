7ebd4b193f9224544d9fd6be54eb5fad3b68fced
Paul Lucas <paul.lucas@fivep.com.au>
2017-05-20 15:54:31 +1000

    x algorithm looking good

--- src/algorithms/rectangleMover.spec.ts
+++ src/algorithms/rectangleMover.spec.ts
@@ -1,7 +1,5 @@
 import * as chai from 'chai';
-
 const { expect } = chai;
-
 
 const layout = (input, mover) => {
 
@@ -10,20 +8,20 @@ const layout = (input, mover) => {
     const item = clone.find(x => x.i == mover.i);
     const currentIndexOfItem = clone.indexOf(item);
 
-
-
     return clone;
 }
-
 
 
 const calculatePosition = (input) => {
     return input;
 }
 
-const runningSum = (input) => {
-    return input.reduce((a, x, i) => [...a, x + (a[i-1] || 0)], []);
-}
+const objectify = (d) => d.map((x, i) => ({ i, w: x }));
+const newItemIndex = (hotspots, mover) => hotspots.indexOf(hotspots.find(h => h.hs <= mover.x && h.he > mover.x));
+const currentItemIndex = (d, m) => d.indexOf(d.find(x => x.i === m.i));
+const runningSum = (i) => i.reduce((a, x, i) => [...a, x + (a[i - 1] || 0)], []);
+const initialPositioning = (i) => i.reduce((a, x, i) => [...a, { ...x, x: (a.length > 0 ? a[i - 1].x + a[i - 1].w : 0) }], []);
+const hotSpots = (d) => d.reduce((a, x, i) => [...a, { ...x, hs: (a.length > 0 ? a[i - 1].he : 0), he: x.x + x.w / 2 }], []);
 
 describe("accumulate", () => {
 
@@ -33,24 +31,54 @@ describe("accumulate", () => {
     });
 });
 
+describe('currentItemIndex', () => {
+
+    let data;
+    beforeEach(() => {
+        data = [100, 100].map((d, i) => ({ i, x: (i * d), w: d }));
+    });
+
+    it('returns correct index', () => {
+        expect(currentItemIndex(data, { i: 0 })).to.eq(0);
+        expect(currentItemIndex(data, { i: 1 })).to.eq(1);
+    });
+});
+
+describe('current positions', () => {
+    it('sets up positions', () => {
+
+        const data = objectify([20, 100, 40, 50]);
+        const expected = [
+            { i: 0, x: 0, w: 20 },
+            { i: 1, x: 20, w: 100 },
+            { i: 2, x: 120, w: 40 },
+            { i: 3, x: 160, w: 50 },
+        ]
+        const result = initialPositioning(data);
+        expect(result[0]).to.deep.eq(expected[0]);
+        expect(result[1]).to.deep.eq(expected[1]);
+        expect(result[2]).to.deep.eq(expected[2]);
+        expect(result[3]).to.deep.eq(expected[3]);
+    });
+});
+
 describe("basics", () => {
 
     let data;
     beforeEach(() => {
-        data = [100, 100].map((d, i) => ({i, x: (i * d), w: d }));
-
+        data = objectify([100, 50, 100]);//.map((d, i) => ({i, w: d }));
     });
 
     it("stays in place", () => {
-        const expected = [{i: 0, x: 0, w: 100}, {i: 1, x: 100, w: 100}];
-        expect(layout(data, {i: 0, x: 0, w: 100})).to.deep.equal(expected);
+        const expected = [{ i: 0, w: 100 }, { i: 1, w: 50 }, { i: 2, w: 100 }];
+        expect(layout(data, { i: 0, x: 0, w: 100 })).to.deep.equal(expected);
     });
 
-    it('stays when only moved a little', () => {
-        const expected = [{i: 0, x: 0, w: 100}, {i: 1, x: 100, w: 100}];
-        expect(layout(data, {i: 0, x: 10, w: 100})).to.deep.eq(expected);
-    });
-    
+    // it('stays when only moved a little', () => {
+    //     const expected = [{i: 0, x: 0, w: 100}, {i: 1, x: 100, w: 100}];
+    //     expect(layout(data, {i: 0, x: 10, w: 100})).to.deep.eq(expected);
+    // });
+
     // it('moves when moved full width', () => {
     //     const expected = [{i: 1, x: 100, w: 100}, {i: 0, x: 0, w: 100}];
     //     expect(layout(data, {i: 0, x: 100, w: 100})).to.deep.eq(expected);
@@ -58,3 +86,24 @@ describe("basics", () => {
 
 
 });
+
+describe('hotspots', () => {
+    it('creates them', () => {
+
+        const data = initialPositioning(objectify([20, 40, 60]));
+        const expected = [{ i: 0, x: 0, w: 20, hs: 0, he: 10 }, { i: 1, x: 20, w: 40, hs: 10, he: 40 }, { i: 2, x: 60, w: 60, hs: 40, he: 90 }]
+        expect(hotSpots(data)).to.deep.eq(expected);
+    });
+
+    it('has appropriate stuff', () => {
+
+        const data = hotSpots(initialPositioning(objectify([20, 40, 60])));
+        const mover = { x: 5}
+        expect(newItemIndex(data, {x: 5})).to.eq(0);
+        expect(newItemIndex(data, {x: 15})).to.eq(1);
+        expect(newItemIndex(data, {x: 10})).to.eq(1);
+        expect(newItemIndex(data, {x: 39})).to.eq(1);
+        expect(newItemIndex(data, {x: 40})).to.eq(2);
+    });
+});
+
